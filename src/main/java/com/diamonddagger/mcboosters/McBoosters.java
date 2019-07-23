fb3086dcff953be39d144c61d9e32869e0f717f2
Coleman Yantis <diamonddagger590@gmail.com>
2019-07-23 07:12:45 -0500

    Initial commit

---
+++ src/main/java/com/diamonddagger/mcboosters/McBoosters.java
@@ -0,0 +1,17 @@
+package com.diamonddagger.mcboosters;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class McBoosters extends JavaPlugin {
+
+  @Override
+  public void onEnable(){
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable(){
+    // Plugin shutdown logic
+  }
+}
