47f8cbc3f6b18314d9e0d72d3d2a384cb0fc628d
DiamondDagger590 <diamonddagger590@gmail.com>
2019-07-25 16:55:06 -0500

    Queue

--- src/main/java/com/diamonddagger/mcboosters/players/BoosterPlayer.java
+++ src/main/java/com/diamonddagger/mcboosters/players/BoosterPlayer.java
@@ -1,8 +1,10 @@
 package com.diamonddagger.mcboosters.players;
 
+import com.diamonddagger.mcboosters.boosters.Booster;
 import lombok.Getter;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
 import java.util.UUID;
 
 public class BoosterPlayer {
@@ -12,4 +14,11 @@ public class BoosterPlayer {
 
     @Getter
     private UUID uuid;
+
+    private HashMap<Booster, Integer> boosters;
+
+    private BoosterPlayer(Player p){
+        this.player = p;
+        this.uuid = p.getUniqueId();
+    }
 }
